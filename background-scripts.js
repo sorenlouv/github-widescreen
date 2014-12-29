a1b7c64a195fa4022b9050f0fcd68f043181c6f5
Søren Louv-Jansen <slo@tradeshift.com>
2014-12-29 18:25:54 +0100

    Cleanup

--- background-scripts.js
+++ background-scripts.js
@@ -1,22 +1,14 @@
 /*globals chrome */
 
 function updateIcon(state){
-	var text, color;
-
-	if (state) {
-		color = [255, 0, 0, 255];
-		text = 'On';
-	} else {
-		color = [190, 190, 190, 230];
-		text = 'Off';
-	}
+	var color = state ? [255, 0, 0, 255] : [190, 190, 190, 230];
+	var text = state ? 'On' : 'Off';
 
 	chrome.browserAction.setBadgeBackgroundColor({color: color});
 	chrome.browserAction.setBadgeText({text: text});
 }
 
 function setState(state){
-	updateIcon(state);
 	chrome.storage.sync.set({state: state});
 }
 
@@ -28,7 +20,9 @@ function getState(callback){
 
 chrome.browserAction.onClicked.addListener(function () {
 	getState(function(state){
-		setState(!state);
+		var newState = !state;
+		updateIcon(newState);
+		setState(newState);
 	});
 });
 
